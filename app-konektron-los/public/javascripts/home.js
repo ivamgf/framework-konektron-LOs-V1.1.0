ae1380dd8868361db4a143c100a00fd5c3240282
Ivam <ivam.developer@gmail.com>
2020-07-07 18:42:22 -0300

    New_Settings_03

--- app-konektron-los/public/javascripts/home.js
+++ app-konektron-los/public/javascripts/home.js
@@ -9,8 +9,8 @@
 // Variables
 
 // Constant
-const home = `<div>
-                <i class="fas fa-graduation-cap fa-7x icon"></i>
+const home = `<div class="container">
+                <i class="fas fa-graduation-cap fa-7x"></i>
                 <h2>Grad Core js</h2>
                 <h3>Framework for learning Objects</h3>
                 <p>Version 1.1.0</p>
